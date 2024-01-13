3bf4c5e6e8a81fe485f0586c1bdea66430bc4dc8
manQaTester <sinm67254@gmail.com>
2024-01-13 16:07:26 +0530

    space with number

--- src/test/java/TestCase_By_ManSingh/Jenkins.java
+++ src/test/java/TestCase_By_ManSingh/Jenkins.java
@@ -3,12 +3,12 @@ package TestCase_By_ManSingh;
 public class Jenkins {
 
 	public static void main(String[] args) {
-		System.out.println("This is jenkins practics1");
-                System.out.println("This is jenkins practics2");
-		System.out.println("This is jenkins practics3");
-		System.out.println("This is jenkins practics4");
-		System.out.println("This is jenkins practics5");
-		System.out.println("This is jenkins practics6");
+		System.out.println("This is jenkins practics - 1");
+		System.out.println("This is jenkins practics - 2");
+		System.out.println("This is jenkins practics - 3");
+		System.out.println("This is jenkins practics - 4");
+		System.out.println("This is jenkins practics - 5");
+		System.out.println("This is jenkins practics - 6");
 	}
 
 }
