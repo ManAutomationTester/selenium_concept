d02f99556ce6ad02a90ac6a45507e0d5ec0cb851
manQaTester <sinm67254@gmail.com>
2024-01-13 16:17:04 +0530

    create one more method

--- src/test/java/TestCase_By_ManSingh/Jenkins.java
+++ src/test/java/TestCase_By_ManSingh/Jenkins.java
@@ -6,6 +6,10 @@ public class Jenkins {
 		System.out.println("This is jenkins practics - 1");
 		System.out.println("This is jenkins practics - 2");
 		System.out.println("This is jenkins practics - 3");
+		
+	}
+	
+	public static void name() {
 		System.out.println("This is jenkins practics - 4");
 		System.out.println("This is jenkins practics - 5");
 		System.out.println("This is jenkins practics - 6");
